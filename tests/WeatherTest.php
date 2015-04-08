f72735bc4974b2db2fc7cf84e2bb0613cabf37f4
Gerald Villacarlos <gerald@yahaay.com>
2015-04-08 18:51:32 +0800

    test scripts

--- tests/WeatherTest.php
+++ tests/WeatherTest.php
@@ -4,29 +4,15 @@
  * @package yhy-weather
  * @subpackage tests
  */
-class BlogEntryTest extends SapphireTest {
+class WeatherTest extends SapphireTest {
     
 	/**
 	* Tests that the blog entry populate defaults works
 	*/
-	// public function testPopulateDefaults() {
-	// 	$member = $this->objFromFixture("Member", "blogOwner1");
-	// 	$member->logIn();
+	public function testPopulateDefaults() {
+		$condition = TRUE;
 
-	// 	// Create manually, as from fixture seems to create entries
-	// 	// multiple times
-	// 	$entry = BlogEntry::create();
-	// 	$entry->Title = "Test post";
-	// 	$entry->URLSegment = "test-post";
-	// 	$entry->Tags = "tag1,tag2";
-
-	// 	// We cant test by the second, as that will most likely fail
-	// 	$now = date('Y-m-d', strtotime('now'));
-
-	// 	$this->assertContains($now, $entry->Date);
-	// 	$this->assertEquals($member->getName(), $entry->Author);
-
-	// 	$member->logOut();
-	// }
+		$this->assertEquals(TRUE, $condition);
+	}
 
 }
