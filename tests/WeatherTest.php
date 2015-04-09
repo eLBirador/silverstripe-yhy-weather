6501325da87c0fbb436b7eef1564f159a99a9d09
Gerald Villacarlos <gerald@yahaay.com>
2015-04-09 13:30:11 +0800

    Test

--- tests/WeatherTest.php
+++ tests/WeatherTest.php
@@ -5,14 +5,29 @@
  * @subpackage tests
  */
 class WeatherTest extends SapphireTest {
+	static $fixture_file = 'yhy-weather/tests/WeatherTest.yml';
+
+	public function setUp() {
+		parent::setUp();
+		
+		//Do not display deprecated errors etc.
+		ini_set('display_errors', 1);
+		ini_set("log_errors", 1);
+		error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
+	}
     
 	/**
 	* Tests that the blog entry populate defaults works
 	*/
 	public function testPopulateDefaults() {
-		$condition = TRUE;
+		$member = $this->objFromFixture("Member", "blogOwner1");
+		$member->logIn();
 
-		$this->assertEquals(TRUE, $condition);
+		$condition = true;
+
+		// $this->assertEquals( true, $condition );
+
+		$member->logOut();
 	}
 
 }
